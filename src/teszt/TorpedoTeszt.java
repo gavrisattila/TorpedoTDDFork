3df375c483bc154f60da5f48e5b4f5e4b597f1e4
gavrisattila <gavrisa01@gmail.com>
2022-05-27 12:26:00 +0200

    teszt sikeres

--- src/teszt/TorpedoTeszt.java
+++ src/teszt/TorpedoTeszt.java
@@ -7,12 +7,21 @@ public class TorpedoTeszt {
         System.out.println("Teszt");
         
         new TorpedoTeszt().tesztLovesTalalt(4);
+        new TorpedoTeszt().tesztLovesNemTalalt(4);
     }
     
     public String tesztLovesTalalt(int poz){
         Hajo hajo = new Hajo(new int[]{2,3,4});
         String t = hajo.talalat(poz);
         assert t.equals("talált") : "nem jó a találat ellenőrzése";
+        return "";
+    }
+    public String tesztLovesNemTalalt(int poz){
+        System.out.println("---mellé");
+        Hajo hajo = new Hajo(new int[]{2,3,4});
+        String t = hajo.talalat(poz);
+        assert t.equals("mellé") : "nem jó a találat ellenőrzése";
+        System.out.println("Mellé teszt sikeres");
         return "";
     }
 }
